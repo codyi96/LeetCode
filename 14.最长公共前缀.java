b6b3ebaa02110540063b8c5a061120e1aef220e7
Charley <471705439@qq.com>
2019-02-28 11:42:48 +0800

    最长公共前缀

---
+++ 14.最长公共前缀.java
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode.cn id=14 lang=java
+ *
+ * [14] 最长公共前缀
+ *
+ * https://leetcode-cn.com/problems/longest-common-prefix/description/
+ *
+ * algorithms
+ * Easy (31.95%)
+ * Total Accepted:    52K
+ * Total Submissions: 162.9K
+ * Testcase Example:  '["flower","flow","flight"]'
+ *
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * 
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * 
+ * 示例 1:
+ * 
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 
+ * 
+ * 说明:
+ * 
+ * 所有输入只包含小写字母 a-z 。
+ * 
+ */
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        if (strs.length == 0) {
+            return result;
+        }
+        for (char c : strs[0].toCharArray()) {
+            String prefix = result + c;
+            for (String str : strs) {
+                if (!str.startsWith(prefix)) {
+                    return result;
+                }
+            }
+            result = prefix;
+        }
+        return result;
+    }
+}
+
