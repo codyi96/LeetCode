0944fe0667532f4ac4bcc34af9f742b2c38734f6
Charley <471705439@qq.com>
2019-04-04 09:50:03 +0800

    只出现一次的数字

---
+++ 136.只出现一次的数字.java
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=java
+ *
+ * [136] 只出现一次的数字
+ *
+ * https://leetcode-cn.com/problems/single-number/description/
+ *
+ * algorithms
+ * Easy (60.09%)
+ * Total Accepted:    55.9K
+ * Total Submissions: 93.1K
+ * Testcase Example:  '[2,2,1]'
+ *
+ * 给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+ * 
+ * 说明：
+ * 
+ * 你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+ * 
+ * 示例 1:
+ * 
+ * 输入: [2,2,1]
+ * 输出: 1
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,1,2,1,2]
+ * 输出: 4
+ * 
+ */
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        for (int num : nums) {
+            // x异或x = 0
+            result ^= num;
+        }
+        return result;
+    }
+}
+
