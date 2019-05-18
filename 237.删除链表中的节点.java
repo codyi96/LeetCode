48220b079b6cfc46b4677ee9576331895931cf21
codyi96 <471705439@qq.com>
2019-05-18 15:24:35 +0800

    删除链表中的节点

---
+++ 237.删除链表中的节点.java
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=237 lang=java
+ *
+ * [237] 删除链表中的节点
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+    }
+}
+
