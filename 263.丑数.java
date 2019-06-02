297b2bb1941642280b0a12680957e933e8731533
codyi96 <471705439@qq.com>
2019-06-02 18:46:59 +0800

    丑数

---
+++ 263.丑数.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=263 lang=java
+ *
+ * [263] 丑数
+ */
+class Solution {
+    public boolean isUgly(int num) {
+        if (num < 1) {
+            return false;
+        }
+        while(num != 1) {
+            if (num % 5 == 0) {
+                num /= 5;
+            } else if (num % 3 == 0) {
+                num /= 3;
+            } else if (num % 2 == 0) {
+                num /= 2;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
