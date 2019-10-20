40d2c70241ff797d362dfb6866153182bb2f43fb
codyi96 <471705439@qq.com>
2019-10-20 15:01:46 +0800

    两个数组的交集

---
+++ 349.两个数组的交集.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * @lc app=leetcode.cn id=349 lang=java
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1 = new HashSet<>();
+        for (int num : nums1) {
+            set1.add(num);
+        }
+
+        HashSet<Integer> resultSet = new HashSet<>();
+        for (int num : nums2) {
+            if (set1.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int index = 0;
+        for(int num : resultSet) {
+            result[index++] = num;
+        }
+        return result;
+    }
+}
+// @lc code=end
+
