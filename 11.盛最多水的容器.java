19ea6e74acec4775fcb9a04c0accb127464f00fc
codyi96 <471705439@qq.com>
2020-08-20 23:33:24 +0800

    11.盛最多水的容器.java

---
+++ 11.盛最多水的容器.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=java
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+class Solution {
+    public int maxArea(int[] height) {
+        int max = 0;
+        int i = 0;
+        int j = height.length - 1;
+        while (i < j) {
+            max = Math.max(max, (j - i) * Math.min(height[i], height[j]));
+            if (height[i] > height[j]) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return max;
+    }
+}
+// @lc code=end
+
