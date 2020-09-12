e1461e19a1e85e3214bdb78d461a1bd2307c8c2c
codyi96 <471705439@qq.com>
2020-09-13 01:57:21 +0800

    48.旋转图像.java

---
+++ 48.旋转图像.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=48 lang=java
+ *
+ * [48] 旋转图像
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return;
+        }
+        int n = matrix[0].length;
+        // 顺时针90°=转置+翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = tmp;
+            }
+        }
+    }
+}
+// @lc code=end
+
