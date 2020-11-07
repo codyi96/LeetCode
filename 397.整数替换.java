b31a8e7e416fd66e1bd60dfdef90ca67f8a26179
codyi96 <471705439@qq.com>
2020-11-08 00:38:25 +0800

    397.整数替换.java

---
+++ 397.整数替换.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=397 lang=java
+ *
+ * [397] 整数替换
+ */
+
+// @lc code=start
+class Solution {
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n == Integer.MAX_VALUE) {
+            return 32;
+        }
+        if (n % 2 == 0) {
+            return integerReplacement(n / 2) + 1;
+        } else {
+            return Math.min(integerReplacement(n + 1),
+            integerReplacement(n - 1)) + 1;
+        }
+    }
+}
+// @lc code=end
+
